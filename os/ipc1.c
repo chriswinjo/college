fa3b15cd7e13e7cfeff2e38c86befff2ca79e461
chriswinjo <u2204021@rajagiri.edu.in>
2024-09-27 11:13:33 +0530

    Add files via upload

---
+++ os/ipc1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<unistd.h>
+int main()
+{
+int fd[2],n;
+char buffer[100],as[100];
+pid_t p;
+pipe(fd);
+p=fork();
+
+if(p>0){
+printf("Parent pass value to child \n");
+printf("Enter data: ");
+fgets(as,100,stdin);
+write(fd[1],as,100);
+}
+
+read(fd[0],buffer,100);
+printf("Child receives data");
+printf("\nBuffer:%s",buffer);
+}
+
